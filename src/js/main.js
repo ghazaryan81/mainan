14e07be1f53d07070bdc1793cc510b0841c9ce0f
ghazaryan81 <hovhannesi.hayk.ghazaryan.81@gmail.com>
2021-10-08 02:17:12 +0400

    start

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+import "./vendor";
+import "./helpers";
+import "./components/social";
+import { ieFix } from "./vendor/ie-fix";
+import { vhFix } from "./vendor/vh-fix";
+import { actualYear } from "./modules/actualYear";
+import header from "./components/header";
+import lazyLoading from "./modules/lazyLoading";
+import scrollToAnchor from "./modules/scrollToAnchor";
+
+ieFix();
+vhFix();
+actualYear();
+scrollToAnchor.init();
+
+header.init();
+lazyLoading.init();
+
+// console.log("object");
+const scrollbar = Scrollbar.init(document.body, { speed: 0.65 });
